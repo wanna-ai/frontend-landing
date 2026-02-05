3e08401378c727eaaaaf4395f12d2583895568b2
wanna-dev <dev@wannna.ai>
2026-02-05 22:50:34 +0100

    add handlegoogle

--- components/LoginProviders/LoginProviders.tsx
+++ components/LoginProviders/LoginProviders.tsx
@@ -2,9 +2,14 @@ import styles from './LoginProviders.module.scss'
 import LoginOAuth from '../LoginOAuth/LoginOAuth'
 
 const LoginProviders = () => {
+
+  const handleGoogle = () => {
+    console.log('handleGoogle')
+  }
+  
   return (
     <div className={styles.loginProviders}>
-      <LoginOAuth _url={'/preview'} />
+      <LoginOAuth _url={'/preview'} handleGoogle={handleGoogle} />
     </div>
   )
 }
