08e8ce9359f53aa0742afeddfc0ad82cf0f95d11
wanna-dev <dev@wannna.ai>
2026-02-10 09:53:38 +0100

    push without redirect

--- app/visibility/page.tsx
+++ app/visibility/page.tsx
@@ -77,7 +77,9 @@ const VisibilityPage = () => {
     <>
       <div className={styles.visibility}>
         <div className={styles.visibility__privacy}>
-          <h2 className={styles.visibility__privacy__title}>Decide quién puede verla</h2>
+          <div className={styles.visibility__privacy__header}>
+            <p className={styles.visibility__privacy__header__title}>Decide quién puede verla:</p>
+          </div>
           <div className={styles.visibility__privacy__options}>
             <label className={styles.visibility__privacy__options__option}>
               <input
@@ -88,8 +90,8 @@ const VisibilityPage = () => {
                 onChange={(e) => setPrivacy(e.target.value as PrivacyOption)}
               />
               <div className={styles.visibility__privacy__options__option__content}>
-                <span className={styles.visibility__privacy__options__option__radio}></span>
-                <div>
+                <div className={styles.visibility__privacy__options__option__radio}></div>
+                <div className={styles.visibility__privacy__options__option__content__text}>
                   <p className={styles.visibility__privacy__options__option__title}>Pública</p>
                   <p className={styles.visibility__privacy__options__option__description}>
                     Visible para todos cuando se lance Wanna
@@ -106,8 +108,8 @@ const VisibilityPage = () => {
                 onChange={(e) => setPrivacy(e.target.value as PrivacyOption)}
               />
               <div className={styles.visibility__privacy__options__option__content}>
-                <span className={styles.visibility__privacy__options__option__radio}></span>
-                <div>
+                <div className={styles.visibility__privacy__options__option__radio}></div>
+                <div className={styles.visibility__privacy__options__option__content__text}>
                   <p className={styles.visibility__privacy__options__option__title}>Privada</p>
                   <p className={styles.visibility__privacy__options__option__description}>
                     Sólo visible para ti y quién tú decidas
@@ -120,11 +122,11 @@ const VisibilityPage = () => {
 
         <div className={styles.visibility__value}>
           <div className={styles.visibility__value__header}>
-            <svg className={styles.visibility__value__header__svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={styles.visibility__value__header__svg} width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M7.26836 14.4091L5.88799 9.25747M7.26836 14.4091L5.01454 15.013M7.26836 14.4091L8.62335 14.5575M5.88799 9.25747L2.34627 10.2065C1.8128 10.3494 1.49622 10.8978 1.63916 11.4312L2.50189 14.651C2.64483 15.1844 3.19317 15.501 3.72663 15.3581L5.01454 15.013M5.88799 9.25747L12.6295 3.22613C13.5296 2.42084 14.6123 1.84693 15.784 1.55402C15.9048 1.52384 16.0279 1.59415 16.0632 1.7135L19.9222 14.75C19.9652 14.8955 19.8827 15.0484 19.7375 15.0922C18.5912 15.4384 17.3864 15.5476 16.1965 15.4131L8.62335 14.5575M5.01454 15.013L7.3621 20.4041C7.55972 20.8579 8.05963 21.0989 8.53776 20.9708L9.83885 20.6222C10.4173 20.4672 10.7307 19.842 10.5089 19.2858L8.62335 14.5575" stroke="var(--color-black)" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M17.8716 6.65375C17.8716 6.65375 19.882 6.2391 20.3779 8.08975C20.8738 9.94039 18.9254 10.5865 18.9254 10.5865" stroke="var(--color-black)" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-            <h2>¿Por qué compartirla?</h2>
+            <p className={styles.visibility__value__header__title}>¿Por qué compartirla?</p>
           </div>
           <p>{experienceData?.story_valuable}</p>
         </div>
@@ -132,7 +134,10 @@ const VisibilityPage = () => {
 
         <div className={styles.visibility__fixed}>
           <button className={styles.visibility__button} onClick={handlePublish}>
-            <p>Finalizar y recibir mi historia</p>
+            <p>Compartir mi historia</p>
+            <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.3536 4.03553C15.5488 3.84027 15.5488 3.52369 15.3536 3.32843L12.1716 0.146446C11.9763 -0.048816 11.6597 -0.048816 11.4645 0.146446C11.2692 0.341708 11.2692 0.658291 11.4645 0.853553L14.2929 3.68198L11.4645 6.51041C11.2692 6.70567 11.2692 7.02225 11.4645 7.21751C11.6597 7.41278 11.9763 7.41278 12.1716 7.21751L15.3536 4.03553ZM0 3.68198L0 4.18198L15 4.18198V3.68198V3.18198L0 3.18198L0 3.68198Z" fill="var(--color-white)"/>
+            </svg>
           </button>
         </div>
       </div>
